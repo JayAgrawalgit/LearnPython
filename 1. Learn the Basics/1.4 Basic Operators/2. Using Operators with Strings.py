9ac8c682927bbc95e3eb357d47d99f2b000901cd
Jay Agrawal <jayeshagrawalgopal@gmail.com>
2020-03-21 15:46:53 +0530

    Basic Operators

---
+++ 1. Learn the Basics/1.4 Basic Operators/2. Using Operators with Strings.py
@@ -0,0 +1,10 @@
+# Python supports concatenating strings using the addition operator:
+helloworld = "hello" + " " + "world"
+print(helloworld)
+# output: hello world
+
+
+# Python also supports multiplying strings to form a string with a repeating sequence:
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+# output:hellohellohellohellohellohellohellohellohellohello
---
+++ 1. Learn the Basics/1.4 Basic Operators/3. Using Operators with Lists.py
@@ -0,0 +1,11 @@
+# Lists can be joined with the addition operators:
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+# output:[1, 3, 5, 7, 2, 4, 6, 8]
+
+
+# Just as in strings, Python supports forming new lists with a repeating sequence using the multiplication operator:
+print([1,2,3] * 3)
+# output:[1, 2, 3, 1, 2, 3, 1, 2, 3]
