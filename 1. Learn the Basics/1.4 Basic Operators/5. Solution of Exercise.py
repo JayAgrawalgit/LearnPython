185fb0b586bd7342634a89e7a77b3f64a411006a
Jay Agrawal <jayeshagrawalgopal@gmail.com>
2020-04-11 20:15:54 +0530

    1.4 Basic Operators

---
+++ 1. Learn the Basics/1.4 Basic Operators/5. Solution of Exercise.py
@@ -0,0 +1,27 @@
+x = object()
+y = object()
+
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+
+print("x_list contains %d objects" % len(x_list))  # x_list contains 10 objects
+print("y_list contains %d objects" % len(y_list))  # y_list contains 10 objects
+print("big_list contains %d objects" % len(big_list))  # big_list contains 20 objects
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")  # Almost there...
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")  # Great!
+
+# output:
+# x_list contains 10 objects
+# y_list contains 10 objects
+# big_list contains 20 objects
+# Almost there...
+# Great!
+
+# here 2 new functions are introduce
+# 1. len: it is use for finding total length of a list
+# 2. Count: it is use for finding count of specific element in list
